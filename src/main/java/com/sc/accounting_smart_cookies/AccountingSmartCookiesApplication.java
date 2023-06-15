c6247a2f2db6c38d19eba8d5ba65bf211a349dfd
owermaiden <ower.maiden@cybertekschool.com>
2023-06-15 13:42:30 -0400

    Initial commit

---
+++ src/main/java/com/sc/accounting_smart_cookies/AccountingSmartCookiesApplication.java
@@ -0,0 +1,13 @@
+package com.sc.accounting_smart_cookies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingSmartCookiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingSmartCookiesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sc/accounting_smart_cookies/Any.java
@@ -0,0 +1,7 @@
+package com.sc.accounting_smart_cookies;
+
+public class Any {
+    public static void main(String[] args) {
+
+    }
+}

9bdea53df4dc3f11665e51334a6d056975b18a95
sefika86 <sefikaakin86@gmail.com>
2023-06-27 11:01:27 -0400

    ClientVendor DTO,Entity,enum Address DTO,Entity

---
+++ src/main/java/com/sc/accounting_smart_cookies/dto/ClientVendorDTO.java
@@ -0,0 +1,24 @@
+package com.sc.accounting_smart_cookies.dto;
+
+import com.sc.accounting_smart_cookies.enums.ClientVendorType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientVendorDTO{
+
+    private Long id;
+    private String clientVendorName;
+    private ClientVendorType clientVendorType;
+    private Long phone;
+    private String website;
+    private AddressDTO addressDTO;
+    private CompanyDTO companyDTO;
+
+}

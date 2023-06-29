7040efc426ffdea397beaf7791393b5a1ff9b336
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-29 10:14:30 -0400

    added product controller

---
+++ src/main/java/com/sc/accounting_smart_cookies/controller/ProductController.java
@@ -0,0 +1,50 @@
+package com.sc.accounting_smart_cookies.controller;
+
+import com.sc.accounting_smart_cookies.dto.ProductDTO;
+import com.sc.accounting_smart_cookies.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public String listProduct(Model model){
+        model.addAttribute("product", productService.findAll());
+        return "product/product-list";
+    }
+    @GetMapping("/create")
+    public String createProduct(Model model){
+        model.addAttribute("product", new ProductDTO());
+        return "product/product-create";
+    }
+    @PostMapping("/create")
+    public String saveProduct(@ModelAttribute("product") ProductDTO productDTO){
+        productService.save(productDTO);
+        return "redirect:/product/product-list";
+    }
+    @GetMapping("/update/{id}")
+    public String editProduct(@PathVariable("id") Long id, Model model){
+        model.addAttribute("project", productService.findById(id));
+        model.addAttribute("products", productService.findAll());
+        return "product/product-update";
+    }
+    @PostMapping("/update")
+    public String updateProduct(@ModelAttribute("product") ProductDTO productDTO){
+        productService.update(productDTO);
+        return "redirect:/product/product-list";
+    }
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable("id") Long id){
+        productService.deleteById(id);
+        return "redirect:/product/product-list";
+    }
+}

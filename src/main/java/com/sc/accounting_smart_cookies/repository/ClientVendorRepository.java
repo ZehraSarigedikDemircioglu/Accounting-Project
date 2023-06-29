5a02c5324cdd6dc149d78da17d129308add59cbf
sefika86 <sefikaakin86@gmail.com>
2023-06-29 12:25:58 -0400

    ClientVendor Basic CRUD

---
+++ src/main/java/com/sc/accounting_smart_cookies/repository/ClientVendorRepository.java
@@ -0,0 +1,7 @@
+package com.sc.accounting_smart_cookies.repository;
+
+import com.sc.accounting_smart_cookies.entity.ClientVendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientVendorRepository extends JpaRepository<ClientVendor, Long> {
+}

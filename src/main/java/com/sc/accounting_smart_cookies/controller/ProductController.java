a71337afe03178c20eb48e616250f1f07640c1de
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-29 22:50:55 -0400

    List Products done

--- src/main/java/com/sc/accounting_smart_cookies/controller/ProductController.java
+++ src/main/java/com/sc/accounting_smart_cookies/controller/ProductController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+
 @Controller
 @RequestMapping("/products")
 public class ProductController {
@@ -18,37 +20,43 @@ public class ProductController {
     }
 
     @GetMapping("/list")
-    public String listProduct(Model model){
+    public String listProduct(Model model) {
         model.addAttribute("products", productService.findAll());
         return "product/product-list";
     }
+
     @GetMapping("/create")
-    public String createProduct(Model model){
+    public String createProduct(Model model) {
         model.addAttribute("newProduct", new ProductDTO());
+        model.addAttribute("productUnits", Arrays.asList(ProductUnit.values()));
+//        model.addAttribute("categories", categoryService.listAll());
         return "product/product-create";
     }
+
     @PostMapping("/create")
-    public String saveProduct(@ModelAttribute("product") ProductDTO productDTO){
+    public String saveProduct(@ModelAttribute("product") ProductDTO productDTO) {
         productService.save(productDTO);
         return "redirect:/products/list";
     }
+
     @GetMapping("/update/{id}")
-    public String editProduct(@PathVariable("id") Long id, Model model){
+    public String editProduct(@PathVariable("id") Long id, Model model) {
         model.addAttribute("product", productService.findById(id));
         model.addAttribute("products", productService.findAll());
-        model.addAttribute("productUnits", ProductUnit.values());
-
+        model.addAttribute("productUnits", Arrays.asList(ProductUnit.values()));
 //        model.addAttribute("categories", categoryService.listAll());
 
         return "product/product-update";
     }
+
     @PostMapping("/update")
-    public String updateProduct(@ModelAttribute("product") ProductDTO productDTO){
+    public String updateProduct(@ModelAttribute("product") ProductDTO productDTO) {
         productService.update(productDTO);
         return "redirect:/products/list";
     }
+
     @GetMapping("/delete/{id}")
-    public String deleteProduct(@PathVariable("id") Long id){
+    public String deleteProduct(@PathVariable("id") Long id) {
         productService.deleteById(id);
         return "redirect:/products/list";
     }

878d9f9cad081ddde74cae7f7605e684c2ef2891
owermaiden <ower.maiden@cybertekschool.com>
2023-06-30 13:06:44 -0400

    security updated

--- src/main/java/com/sc/accounting_smart_cookies/service/SecurityService.java
+++ src/main/java/com/sc/accounting_smart_cookies/service/SecurityService.java
@@ -1,6 +1,11 @@
 package com.sc.accounting_smart_cookies.service;
 
+import com.sc.accounting_smart_cookies.dto.UserDTO;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface SecurityService extends UserDetailsService {
+
+    UserDTO getLoggedInUser();
 }

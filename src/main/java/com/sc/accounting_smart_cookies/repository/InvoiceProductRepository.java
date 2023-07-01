2938cddec751445cab01376bf3c653acb7ad0ad3
Ivan_Cydeo <remezivan1@gmail.com>
2023-07-01 13:28:59 -0400

    Ivan - SMART-52 update

--- src/main/java/com/sc/accounting_smart_cookies/repository/InvoiceProductRepository.java
+++ src/main/java/com/sc/accounting_smart_cookies/repository/InvoiceProductRepository.java
@@ -11,4 +11,5 @@ import java.util.List;
 @Repository
 public interface InvoiceProductRepository extends JpaRepository<InvoiceProduct, Long> {
 
+    List<InvoiceProduct> findAllByInvoice_Id(Long invoiceId);
 }

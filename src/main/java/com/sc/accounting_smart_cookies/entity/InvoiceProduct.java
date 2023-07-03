8fcdaff432c623961b4b4ad974a177288d751ff1
Malik_Git <malikganiev2002@gmail.com>
2023-07-03 11:37:42 -0400

    updates

--- src/main/java/com/sc/accounting_smart_cookies/entity/InvoiceProduct.java
+++ src/main/java/com/sc/accounting_smart_cookies/entity/InvoiceProduct.java
@@ -22,7 +22,7 @@ public class InvoiceProduct extends BaseEntity {
     private BigDecimal price;
     private int tax;
     private BigDecimal profitLoss;
-    private int remainingQuantity;
+    private Integer remainingQuantity;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Invoice invoice;

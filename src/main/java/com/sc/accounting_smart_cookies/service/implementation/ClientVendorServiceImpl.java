ad9d2c396878194e5370797b93944d97bffe2398
owermaiden <ower.maiden@cybertekschool.com>
2023-07-04 23:13:50 -0400

    problem solved

--- src/main/java/com/sc/accounting_smart_cookies/service/implementation/ClientVendorServiceImpl.java
+++ src/main/java/com/sc/accounting_smart_cookies/service/implementation/ClientVendorServiceImpl.java
@@ -1,14 +1,16 @@
 package com.sc.accounting_smart_cookies.service.implementation;
 
 import com.sc.accounting_smart_cookies.dto.ClientVendorDTO;
-import com.sc.accounting_smart_cookies.dto.InvoiceDTO;
 import com.sc.accounting_smart_cookies.entity.ClientVendor;
-import com.sc.accounting_smart_cookies.entity.Invoice;
+import com.sc.accounting_smart_cookies.entity.Company;
 import com.sc.accounting_smart_cookies.enums.ClientVendorType;
 import com.sc.accounting_smart_cookies.mapper.MapperUtil;
 import com.sc.accounting_smart_cookies.repository.ClientVendorRepository;
 import com.sc.accounting_smart_cookies.service.ClientVendorService;
+import com.sc.accounting_smart_cookies.service.CompanyService;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -19,10 +21,13 @@ public class ClientVendorServiceImpl implements ClientVendorService {
     private final ClientVendorRepository clientVendorRepository;
     private final MapperUtil mapperUtil;
 
+    private final CompanyService companyService;
 
-    public ClientVendorServiceImpl(ClientVendorRepository clientVendorRepository, MapperUtil mapperUtil) {
+
+    public ClientVendorServiceImpl(ClientVendorRepository clientVendorRepository, MapperUtil mapperUtil, CompanyService companyService) {
         this.clientVendorRepository = clientVendorRepository;
         this.mapperUtil = mapperUtil;
+        this.companyService = companyService;
     }
 
 
@@ -36,8 +41,14 @@ public class ClientVendorServiceImpl implements ClientVendorService {
 
     @Override
     public List<ClientVendorDTO> findAll() {
-        List<ClientVendor> clientVendors = clientVendorRepository.getClientVendorsSortedByTitleAndName();
-        return clientVendors.stream().map(clientVendor1 -> mapperUtil.convert(clientVendor1, new ClientVendorDTO())).collect(Collectors.toList());
+        Company company = mapperUtil.convert(companyService.getCompanyOfLoggedInUser(), new Company());
+        return clientVendorRepository
+                .findAllByCompany(company).stream()
+                .sorted(Comparator.comparing(ClientVendor::getClientVendorType)
+                        .reversed()
+                        .thenComparing(ClientVendor::getClientVendorName))
+                .map(cv ->  mapperUtil.convert(cv, new ClientVendorDTO()))
+                .collect(Collectors.toList());
     }
 
 

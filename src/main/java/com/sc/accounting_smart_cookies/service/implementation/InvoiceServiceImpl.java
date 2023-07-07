8ca4c0d530907730924937ddfd2b117ab5b274da
Ivan_Cydeo <remezivan1@gmail.com>
2023-07-06 22:22:25 -0400

    feature_SMART-61 - update

--- src/main/java/com/sc/accounting_smart_cookies/service/implementation/InvoiceServiceImpl.java
+++ src/main/java/com/sc/accounting_smart_cookies/service/implementation/InvoiceServiceImpl.java
@@ -143,6 +143,7 @@ public class InvoiceServiceImpl implements InvoiceService {
         if (invoice.isPresent()) {
             invoice.get().setInvoiceStatus(InvoiceStatus.APPROVED);
             invoice.get().setDate(LocalDate.now());
+//            invoiceProductService.completeApproval(id, invoice.get().getInvoiceType());
             invoiceRepository.save(invoice.get());
         }
 

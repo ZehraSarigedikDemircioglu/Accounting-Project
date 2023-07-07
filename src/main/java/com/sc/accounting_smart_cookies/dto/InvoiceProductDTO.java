ce45bc0651e72a530acfb8eeecaab27b6ce59574
Ivan_Cydeo <remezivan1@gmail.com>
2023-07-07 17:33:54 -0400

    feature_SMART-59 - update

--- src/main/java/com/sc/accounting_smart_cookies/dto/InvoiceProductDTO.java
+++ src/main/java/com/sc/accounting_smart_cookies/dto/InvoiceProductDTO.java
@@ -14,18 +14,22 @@ import java.math.BigDecimal;
 @Builder
 public class InvoiceProductDTO {
 
-   private Long id;
-   @NotNull(message = "Quantity is a required field.")
-   private Integer quantity;
-   @NotNull(message = "Price is a required field.")
-   private BigDecimal price;
-   @NotNull(message = "Tax is a required field.")
-   private Integer tax;
-   private BigDecimal total;
+    private Long id;
+    @NotNull(message = "Quantity is a required field.")
+    @Range(min = 1, max = 100, message = "Quantity cannot be greater than 100 or less than 1")
+    private Integer quantity;
+    @NotNull(message = "Price is a required field.")
+    @Range(min = 1, message = "Price should be at least $1")
+    private BigDecimal price;
+    @NotNull(message = "Tax is a required field.")
+    @Range(min = 0, max = 20, message = "Tax should be between 0% and 20%")
+    private Integer tax;
+    private BigDecimal total;
 
-   private BigDecimal profitLoss;
-   private Integer remainingQty;
-   private InvoiceDTO invoice;
-   @NotNull(message = "Product is a required field.")
-   private ProductDTO product;
+    private BigDecimal profitLoss;
+    private Integer remainingQty;
+    private InvoiceDTO invoice;
+    @Valid
+    @NotNull(message = "Product is a required field.")
+    private ProductDTO product;
 }

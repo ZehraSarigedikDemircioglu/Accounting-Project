59d44aacc20e3659294053623265f83619f48537
owermaiden <ower.maiden@cybertekschool.com>
2023-07-12 09:53:45 -0400

    repository

--- src/main/java/com/sc/accounting_smart_cookies/controller/StockReportController.java
+++ src/main/java/com/sc/accounting_smart_cookies/controller/StockReportController.java
@@ -1,5 +1,6 @@
 package com.sc.accounting_smart_cookies.controller;
 
+import com.sc.accounting_smart_cookies.service.ReportingService;
 import com.sc.accounting_smart_cookies.service.StockReportService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,11 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class StockReportController {
 
     final private StockReportService stockReportService;
+    private final ReportingService reportingService;
 
-    public StockReportController(StockReportService stockReportService) {
+    public StockReportController(StockReportService stockReportService, ReportingService reportingService) {
         this.stockReportService = stockReportService;
+        this.reportingService = reportingService;
     }
-
 
     @GetMapping("/stockData")
     public String listApprovedInvoiceList (Model model) {
@@ -24,6 +26,12 @@ public class StockReportController {
         model.addAttribute("invoiceProducts",stockReportService.generateStockReport());
 
         return "report/stock-report";
+    }
+
+    @GetMapping("/profitLossData")
+    public String getAllProfitLossReport(Model model) {
+        model.addAttribute("monthlyProfitLossDataMap", reportingService.listMonthlyProfitLoss());
+        return "report/profit-loss-report";
     }
 
 
